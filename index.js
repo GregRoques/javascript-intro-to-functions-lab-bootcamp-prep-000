e8d8cf6a67e4d2887c1aad2f19486b7e6d531f42
Greg Roques <greg@gregroques.com>
2018-08-16 20:19:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){ 
-  if (string === string.toLowerCase()){
-    console.log("I can\'t hear you!");
+  if (string === string.toUpperCase()){
+    console.log("YES INDEED!"); 
   }
-  else if (string === string.toUpperCase()){
-    console.log("YES INDEED!");
+  else if (string === string.toowerCase()){
+    console.log("I can\'t hear you!");
   }
   else if (string === "I love you, Grandma."){
     console.log("I love you, too.");
