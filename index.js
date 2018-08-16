5a1eedc3bc087f91e3b666793a246da19ddd7078
Greg Roques <greg@gregroques.com>
2018-08-16 20:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!";
   }
   else if (string === string.toUpperCase()){
-    console.log("YES INDEED!");
+    return "YES INDEED!";
   }
   else if (string === "I love you, Grandma."){
-    console.log("I love you, too.");
+    return "I love you, too.";
   }
 }
 
